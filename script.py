b987b4ca320aa9a584ff34cc6d2328f07d2b638e
TheLipschitz <66444597+TheLipschitz@users.noreply.github.com>
2020-09-24 16:23:02 -0500

    Add function to plot a single roller coaster's rank over time

--- script.py
+++ script.py
@@ -28,16 +28,41 @@ print(wood_data.head(), steel_data.head())
 
 # write function to plot rankings over time for 1 roller coaster here:
 
+def ranking_over_time(coaster: str, park: str, rank_data: pd.DataFrame):
+    filtered_rankings = rank_data[(rank_data['Name'] == coaster) & (rank_data['Park'] == park)]
+    print(park)
+    coaster_years = list(filtered_rankings['Year of Rank'])
+    coaster_ranks = list(filtered_rankings['Rank'])
+
+    rank_ticks = sorted(list(set(coaster_ranks)))
+
+    ax = plt.subplot()
+    plt.plot(coaster_years, coaster_ranks, marker='o')
+    plt.xlabel('Year')
+    plt.ylabel('Rank')
+    plt.title(f'{coaster} Rankings by Year')
+    plt.xticks(coaster_years)
+    plt.yticks(rank_ticks)
+    ax.invert_yaxis()
+    plt.subplots_adjust(bottom=0.2)
 
 
-
-
-
-
-
-
+# plt.subplot(1, 2, 1)
+# ranking_over_time('Lightning Racer', 'Hersheypark', wood_data)
+# plt.subplot(1, 2, 2)
+ranking_over_time('Millennium Force', 'Cedar Point', steel_data)
+plt.show()
 
 plt.clf()
+
+# 4.
+# Write a function that will plot the ranking of two given roller coasters over time as lines. Your function should take
+# both roller coasters’ names and a ranking DataFrame as arguments. Make sure to include informative labels that
+# describe your visualization.
+#
+# Call your function with "El Toro" as one roller coaster name, “Boulder Dash“ as the other roller coaster name, and the
+# wood ranking DataFrame. What issue do you notice? Update your function with two additional arguments to alleviate the
+# problem, and retest your function.
 
 # write function to plot rankings over time for 2 roller coasters here:
 
